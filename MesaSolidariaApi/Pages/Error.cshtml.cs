3490a8806acbc858ac46fedabfc2e650852b884a
Dara Kimberly de Souza <darakimberlys@hotmail.com>
2024-01-25 19:26:55 -0300

    update api

--- MesaSolidariaApi/Pages/Error.cshtml.cs
+++ MesaSolidariaApi/Pages/Error.cshtml.cs
@@ -2,21 +2,22 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace mesa_solidaria_api.Pages;
+namespace MesaSolidariaApi.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
     private readonly ILogger<ErrorModel> _logger;
 
     public ErrorModel(ILogger<ErrorModel> logger)
     {
         _logger = logger;
     }
-
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     public void OnGet()
     {
--- MesaSolidariaApi/Program.cs
+++ MesaSolidariaApi/Program.cs
@@ -1,29 +1,25 @@
-using MassTransit;
-using MesaSolidariaApi.IoC;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var configuration = builder.Configuration;
-
-builder.Services.AddDataBaseConnection(configuration);
-builder.Services.AddPubSubConfiguration(configuration);
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
